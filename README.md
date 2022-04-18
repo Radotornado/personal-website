e83844e64a9f9f41e5cc3706b480e105dff2aaf2
Radoslav Mandev <radoslav.mandev99@gmail.com>
2022-04-18 18:32:05 +0200

    Changed loading and expanded Readme

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-# TODO
+# My personal website 
+### [Demo](http://mandev.net)
 
-## Running Locally
+This is the repository, where I built my personal website using the *Model–view–viewmodel* pattern. The libraries and tools I utilized were: 
+- [Vue.js](https://github.com/vuejs/vue) as main framework;
+- [Yarn](https://github.com/yarnpkg/yarn) for package management;
+- [Vercel](https://github.com/vercel/vercel) for deployment;
+- [vue-router](https://github.com/vuejs/vue-router) for scrolling and routing;
+- [i18n](https://github.com/kazupon/vue-i18n) for internationalization;
+- [vuetify](https://github.com/vuetifyjs/vuetify) for material design;
+- [SASS](https://github.com/sass/sass) for global variables; 
+- [SpinKit](https://github.com/tobiasahlin/SpinKit) for the loader.  
 
-```
-yarn install
-```
+# Running Locally
 
-#### Compile and hot-reload for development
-
-```
-yarn serve
-```
-
-#### Compile and minify for production
-
-```
-yarn build
-```
-
-#### Lint and fix files
-
-```
-yarn lint
-```
+For deployment use: ```yarn install```
+To compile and hot-reload for development: ```yarn serve```
+To compile and minify for production: ```yarn build```
+Finally for analysis and fixing issues: ```yarn lint```
--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,26 +1,84 @@
+<!-- Thanks to @tobiasahlin for the loading animation https://tobiasahlin.com / https://github.com/tobiasahlin/SpinKit -->
+
 <template>
   <v-container fill-height align-center justify="center">
     <v-row>
       <v-col cols="12" align="center">
-        <v-img
-          alt="Radoslav Mandev"
-          class="shrink mr-2"
-          contain
-          :src="require('@/assets/favicon.svg')"
-          transition="scale-transition"
-          width="200"
-        />
+        <div class="spinner">
+          <div class="rect1"></div>
+          <div class="rect2"></div>
+          <div class="rect3"></div>
+          <div class="rect4"></div>
+          <div class="rect5"></div>
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<style scoped>
+.spinner {
+  margin: 100px auto;
+  width: 100px;
+  height: 80px;
+  text-align: center;
+  font-size: 10px;
+}
+
+.spinner > div {
+  background-color: #333;
+  height: 100%;
+  width: 8px;
+  display: inline-block;
+  margin-right: 1px;
+  
+  -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
+  animation: sk-stretchdelay 1.2s infinite ease-in-out;
+}
+
+.spinner .rect2 {
+  -webkit-animation-delay: -1.1s;
+  animation-delay: -1.1s;
+}
+
+.spinner .rect3 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+
+.spinner .rect4 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+
+.spinner .rect5 {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+
+@-webkit-keyframes sk-stretchdelay {
+  0%, 40%, 100% { -webkit-transform: scaleY(0.4) }  
+  20% { -webkit-transform: scaleY(1.0) }
+}
+
+@keyframes sk-stretchdelay {
+  0%, 40%, 100% { 
+    transform: scaleY(0.4);
+    -webkit-transform: scaleY(0.4);
+  }  20% { 
+    transform: scaleY(1.0);
+    -webkit-transform: scaleY(1.0);
+  }
+}
+</style>
+
 <script>
 export default {
     name:"Loading",
     mounted: function(){
         setTimeout(() =>{
             this.$router.push("/main");
-        },1000);
+        },600);
     }
 };
 </script>
